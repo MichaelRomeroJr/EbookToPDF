11b3e2b9be3c3551b08beb63003db889698585cd
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2019-09-18 13:46:09 -0700

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -3,6 +3,16 @@ import os
 import TurnPage
 
 def Requirements():      
+    """Confirm dependent libraries"""
+    try: 
+        import mss
+    except:
+        print("import mss fail")
+    try:
+        import PyUserInput
+    except:
+        print("import PyUserInput fail")
+    
     """Create save directory for images and pdf """
     PathToDesktop = input("Enter path to desktop: ")
     DirectoryPath = PathToDesktop + '/NewFolder/'
@@ -10,11 +20,11 @@ def Requirements():
     if not os.path.exists(DirectoryPath):
         os.makedirs(DirectoryPath)
 
-    #Number of pages of pdf
+    """Get number of pages of eBook"""
     NumberofPages = input("Enter number of pages: ")
     PageCount = int(NumberofPages)
     
-    #Get position of "Turn Page" button 
+    """Get positino of 'turn page' button of eBook"""
     print("Click 'next page' button on eReader to identify x,y coordinates" +
           "(click next page once then toggle back to cover page of book)")
     XCoordinate, YCoordinate = TurnPage.GetTurnPageCoordinates()
